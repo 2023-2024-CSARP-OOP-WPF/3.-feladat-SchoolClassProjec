ceb7c80eb5a036979f58058745ba332394eefa5d
2020b-Szabó Marcell <74899044+b0szamar@users.noreply.github.com>
2024-05-27 16:20:23 +0200

    Delete SchoolClassProject directory

--- SchoolClassProject/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using SchoolClassProject;
-
-SchoolClass aClass = new SchoolClass(9, ".a", 7000);
-SchoolClass bClass = new SchoolClass(9, ".b", 6000);
-Console.WriteLine(aClass.ToString());
-Console.WriteLine(bClass.ToString());
